2d155fcca5eb5b18e090970ada495b9a697fffcb
JadeSnow7 <1079249368@qq.com>
2025-12-05 14:48:44 +0800

    Remove unused QueueFRPTOS and fastapi stub

--- QueueFRPTOS/frp_config/local/start_frpc_worker.ps1
+++
@@ -1,9 +0,0 @@
-Get-Content .env | ForEach-Object {
-    $line = $_.Trim()
-    if ($line -and -not $line.StartsWith("#")) {
-        $name, $value = $line -split '=', 2
-        [Environment]::SetEnvironmentVariable($name, $value, "Process")
-    }
-}
-
-.\frpc.exe -c .\frpc_worker.toml
